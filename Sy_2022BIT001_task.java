2df3dd0b92b9e6b82ad6f553a4f4e9c9fab37013
pushkar <2022bit001@sggs.ac.in>
2024-01-29 00:57:43 +0530

    Verify Registration number program

---
+++ Sy_2022BIT001_task.java
@@ -0,0 +1,29 @@
+public class Sy_2022BIT001_task {
+    public static void main(String[] args) {
+        Character arr[]={'2','0','2','2','B','I','T','0','0','1'};
+        if(verifyRegistrationNum(arr)){
+            System.out.println("valid");
+        }
+        else{
+            System.out.println("invalid");
+        }
+    }
+    public static boolean verifyRegistrationNum(Character arr[]){
+        for(int i=0;i<=arr.length;i++){
+            if(i<4||(i>=7&&i<10)){
+                boolean b=Character.isDigit(arr[i]);
+                if(!b){
+                    return false;
+                }
+                }
+                if(i>=4&& i<7){
+                    boolean c= Character.isAlphabetic(arr[i]);
+                    if(!c){
+                        return false;
+                    }
+                    }
+                }
+                return true;
+    }
+ }
+    
